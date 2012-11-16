3b769e0011d1e547823961079f113c0a0d907462
jtimberman <joshua@opscode.com>
2012-11-15 19:17:54 -0700

    release v1.0.6

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## v1.0.6:
+
+* Update to version 3.2.8
+
 ## v1.0.4:
 
 * Add license file
--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,8 @@
+name             "tunnelblick"
 maintainer       "Joshua Timberman"
 maintainer_email "cookbooks@housepub.org"
 license          "Apache 2.0"
 description      "Installs/Configures tunnelblick"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.4"
+version          "1.0.6"
 depends          "dmg"
