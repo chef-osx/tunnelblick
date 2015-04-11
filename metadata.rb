64a3b5c389b21a7c9d3228be3602f5df94e3b912
Brian Hays <brian.hays@gmail.com>
2015-04-11 17:20:03 -0400

    v1.1.0 - update to version 3.4.3 (build 4055.4198)

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name             "tunnelblick"
-maintainer       "Joshua Timberman"
-maintainer_email "cookbooks@housepub.org"
+maintainer       "Brian Hays"
+maintainer_email "brian.hays@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures tunnelblick"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.6"
+version          "1.1.0"
 depends          "dmg"
